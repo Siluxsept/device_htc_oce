b87ec386d934f7d85524bac2a900b1906092f66a
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    oce: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- device.mk
+++ device.mk
@@ -419,6 +419,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/product/etc/permissions/privapp-permissions-oem.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
