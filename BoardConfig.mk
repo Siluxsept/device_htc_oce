97e057b7b97068b2f4910d9e12290aedbd7002d7
TARKZiM <tom8476oo@gmail.com>
2020-10-12 23:53:59 +0800

    oce: Update board kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 BOARD_VENDOR := htc
 
 DEVICE_PATH := device/htc/oce
@@ -45,9 +44,6 @@ TARGET_USES_64_BIT_BINDER := true
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
-# Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := true
-
 # Bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
@@ -65,6 +61,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/htc/oce
 TARGET_KERNEL_CONFIG := oce_defconfig
 TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
